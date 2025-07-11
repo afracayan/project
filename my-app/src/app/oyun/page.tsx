d816fed326ec52e410ac08c4ce5cd579cc3bf3c8
afracayan <afra.cayan@gmail.com>
2025-07-11 16:29:39 +0300

    tohum  büyümesi ve çürüme aşaması

--- my-app/src/app/oyun/page.tsx
+++ my-app/src/app/oyun/page.tsx
@@ -5,9 +5,12 @@ import styles from "./tarla.module.css";
 export default function Home() {
   const [seeds, setSeeds] = useState(Array(16).fill(""));
 
-  function plantSeed(index: number) {
+  function handleGrowing(index: number) {
+
+
     setSeeds((prevSeeds) => {
       const newSeeds = [...prevSeeds];
+      
       if (newSeeds[index] === "") {
         newSeeds[index] = "T";
       } else if (newSeeds[index] === "T") {
@@ -18,20 +21,38 @@ export default function Home() {
         newSeeds[index] = "Ç";
       } else if (newSeeds[index] === "Ç") {
         newSeeds[index] = "K";
-      } else if (newSeeds[index] === "K"){
+      } else if (newSeeds[index] === "K") {
         newSeeds[index] = "";
-        }
-
-       
-        
-      
-      
-          
-
+      }
       return newSeeds;
     });
+    
+    const tToF = setTimeout(() => {
+      setSeeds((prev) => prev.map((s) => (s === "T" ? "F" : s)));
+    }, 2000);
+
+    const fToB = setTimeout(() => {
+      setSeeds((prev) => prev.map((s) => (s === "F" ? "B" : s)));
+    }, 4000);
+
+    const bToC = setTimeout(() => {
+      setSeeds((prev) => prev.map((s) => (s === "B" ? "Ç" : s)));
+    }, 6000);
+
+    const cToK = setTimeout(() => {
+      setSeeds((prev) => prev.map((s) => (s === "Ç" ? "K" : s)));
+    }, 10000);
+
+    
+
+    return () => {
+      clearTimeout(tToF);
+      clearTimeout(fToB);
+      clearTimeout(bToC);
+      clearTimeout(cToK);
+      ;
+    };
   }
-  
 
   return (
     <div
@@ -45,8 +66,9 @@ export default function Home() {
       {seeds.map((seed, index) => (
         <div
           key={index}
-          onClick={() => plantSeed(index)}
+          onClick={() => handleGrowing(index)}
           className={styles.parsel}
+          style={{ cursor: "pointer", userSelect: "none" }}
         >
           {seed}
         </div>
