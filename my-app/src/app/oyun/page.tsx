5019a9e1db9706b550ea07ddd9d16c1788ffbd83
afracayan <afra.cayan@gmail.com>
2025-07-11 14:36:35 +0300

    4.aşama Manuel büyümesi

--- my-app/src/app/oyun/page.tsx
+++ my-app/src/app/oyun/page.tsx
@@ -1,37 +1,56 @@
 "use client";
-import styles from "@/app/oyun/tarla.module.css";
+import { useState } from "react";
+import styles from "./tarla.module.css";
 
 export default function Home() {
+  const [seeds, setSeeds] = useState(Array(16).fill(""));
+
+  function plantSeed(index: number) {
+    setSeeds((prevSeeds) => {
+      const newSeeds = [...prevSeeds];
+      if (newSeeds[index] === "") {
+        newSeeds[index] = "T";
+      } else if (newSeeds[index] === "T") {
+        newSeeds[index] = "F";
+      } else if (newSeeds[index] === "F") {
+        newSeeds[index] = "B";
+      } else if (newSeeds[index] === "B") {
+        newSeeds[index] = "Ç";
+      } else if (newSeeds[index] === "Ç") {
+        newSeeds[index] = "K";
+      } else if (newSeeds[index] === "K"){
+        newSeeds[index] = "";
+        }
+
+       
+        
+      
+      
+          
+
+      return newSeeds;
+    });
+  }
+  
+
   return (
-    <>
-      <div className={styles.parsel}>
-        <div className={styles.parsel2}>
-          <div className={styles.parsel2}>
-            <div className={styles.parsel2}></div>
-          </div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 110px)",
+        gap: "8px",
+        justifyContent: "center",
+      }}
+    >
+      {seeds.map((seed, index) => (
+        <div
+          key={index}
+          onClick={() => plantSeed(index)}
+          className={styles.parsel}
+        >
+          {seed}
         </div>
-      </div>
-      <div className={styles.parsel}>
-        <div className={styles.parsel2}>
-          <div className={styles.parsel2}>
-            <div className={styles.parsel2}></div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.parsel}>
-        <div className={styles.parsel2}>
-          <div className={styles.parsel2}>
-            <div className={styles.parsel2}></div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.parsel}>
-        <div className={styles.parsel2}>
-          <div className={styles.parsel2}>
-            <div className={styles.parsel2}></div>
-          </div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
