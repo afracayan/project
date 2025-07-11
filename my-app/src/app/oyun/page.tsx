a666158e9189d8439e7fed7ab93abdb813a989c4
afracayan <afra.cayan@gmail.com>
2025-07-11 12:05:13 +0300

    2.aşama

--- my-app/src/app/oyun/page.tsx
+++ my-app/src/app/oyun/page.tsx
@@ -1,13 +1,37 @@
 "use client";
-export default function Home() {
- 
- 
-  return (
-    <div >
-        oyun
+import styles from "@/app/oyun/tarla.module.css";
 
-      
-      
-    </div>
+export default function Home() {
+  return (
+    <>
+      <div className={styles.parsel}>
+        <div className={styles.parsel2}>
+          <div className={styles.parsel2}>
+            <div className={styles.parsel2}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.parsel}>
+        <div className={styles.parsel2}>
+          <div className={styles.parsel2}>
+            <div className={styles.parsel2}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.parsel}>
+        <div className={styles.parsel2}>
+          <div className={styles.parsel2}>
+            <div className={styles.parsel2}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.parsel}>
+        <div className={styles.parsel2}>
+          <div className={styles.parsel2}>
+            <div className={styles.parsel2}></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
